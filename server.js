bb55bd08c3de5286dab14327c7145d581ada2054
Amritesh Pandey <amritesh@Amriteshs-MacBook-Air.local>
2025-05-26 01:05:56 +0530

    cors fixed

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173' || 'https://frontend-lapz.onrender.com/' ,
+  origin: 'http://localhost:5173' || 'https://frontend-lapz.onrender.com' ,
   credentials: true,
 }));
 app.use(express.json());
